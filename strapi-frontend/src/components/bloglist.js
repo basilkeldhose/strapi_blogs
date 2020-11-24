b2bf3a154448c96c88a7fbae5c11126cac60dd0e
basilKeldhose <beldhose2015@gmail.com>
2020-11-24 12:02:34 +0530

    second commit

--- strapi-frontend/src/components/bloglist.js
+++ strapi-frontend/src/components/bloglist.js
@@ -2,37 +2,37 @@ import React, { Component } from 'react';
 import Bloglistitem from './bloglistitem'
 import axios from 'axios'
 
-class Bloglist extends Component{
-    constructor(){
+class Bloglist extends Component {
+    constructor() {
         super()
-        this.state={
-            blogs:[]
+        this.state = {
+            blogs: []
         }
     }
 
-    componetWillMount(){
-        axios.get('http://localhost:1337/Blogs').then((response)=>{
-            if(response.status >=400){
+    componetDidMount() {
+        axios.get('http://localhost:1337/Blogs').then((response) => {
+            if (response.status >= 400) {
                 throw new Error("No respnse from server")
             }
-            return response.json();
-        }).then((blogs)=>{
-            this.setState({blogs:blogs});
+            console.log(response);
+        }).then((Blogs) => {
+            this.setState({ blogs: Blogs });
         })
 
     }
-    render(){
-        return(
+    render() {
+        return (
             <div>
                 {
-                    this.state.blogs.map(({id,blogtitile,blogcontent,featuredimage,categories})=>(
-                    <div className="container">
-                        <div className="row">
-                <Bloglistitem key={id}blogid={id} blogtitile={blogtitile} blogcontent={blogcontent} featuredimage={featuredimage} categories ={categories}/>
+                    this.state.blogs.map(({ id, blogtitile, blogcontent, featuredimage, categories }) => (
+                        <div className="container">
+                            <div className="row">
+                                <Bloglistitem key={id} blogid={id} blogtitile={blogtitile} blogcontent={blogcontent} featuredimage={featuredimage} categories={categories} />
 
+                            </div>
                         </div>
-                    </div>
-        ))
+                    ))
                 }
 
             </div>
--- strapi-frontend/src/components/navigation.js
+++ strapi-frontend/src/components/navigation.js
@@ -1,10 +1,10 @@
-import React, { Component } from 'react'
+import React from 'react'
 
 const Navbar =()=>{
     return(
         <div>
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-                <a className="navbar-brand" href="">Sample Blog</a>
+                <a className="navbar-brand" href="/single">Sample Blog</a>
                 <a className="navbar-brand" href="/addblog">Add New Blog</a>
             </nav>
         </div>
